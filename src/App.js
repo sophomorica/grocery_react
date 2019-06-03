99e1c24b5912787d0063358f750f7266aa07c83d
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-03 15:43:57 -0600

    starting my app class and components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      g_list: [
+        {id: 1, name: "Milk", bought: false,},
+        {id: 1, name: "bread", bought: true,},
+        {id: 1, name: "cheese", bought: true,},
+      ]
+    }
+  }
+  render(){
+    return(
+      <div><h1>
+        Grocery List
+        </h1>
+        
+        </div>
+    )
+  }
 }
 
 export default App;
